3d4ba27050d26f1666c67523ff8ccb5a8393750e
yuumm <15802883812@163.com>
2022-11-12 23:10:15 +0800

    ‘init’

---
+++ src/main/java/com/cim/request/JavaRequestApplication.java
@@ -0,0 +1,17 @@
+package com.cim.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@Slf4j
+public class JavaRequestApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(JavaRequestApplication.class, args);
+        log.info("项目启动成功...");
+    }
+
+}
---
+++ src/test/java/com/cim/request/JavaRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cim.request;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRequestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
